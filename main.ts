3769a5a0cb578cdd3bde4d081e1ecaf63c7b03b0
vili116 <36635527+vilemek@users.noreply.github.com>
2023-09-07 16:10:37 +0200

    Base code

--- main.ts
+++ main.ts
@@ -1,1 +1,48 @@
-
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 99) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # . . .
+            . . . . .
+            . . . . .
+            `)
+        radio.sendNumber(98)
+    }
+    if (receivedNumber == 98) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # . # .
+            . . . . .
+            . . . . .
+            `)
+        radio.sendNumber(100)
+    }
+    if (receivedNumber == 100) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # # # .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    letter += 1
+    basic.showString(convertToText(letter))
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString("" + convertToText(letter))
+    letter = 0
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+let letter = 0
+radio.setGroup(1)
+radio.sendNumber(99)
